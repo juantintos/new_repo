6bf214f573ee3b3838b502fa8c23381497291aec
JuanTintos <juantintos@gmail.com>
2025-02-24 17:42:17 -0600

    Unicafe

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,57 +1,50 @@
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+import { useState } from 'react'
 
+const StaticLine = ({ text, value }) => {
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <td>{text}</td>
+      <td>{value}</td>
     </div>
   )
 }
 
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>
-  )
-}
-
-const Content = ({ parts }) => {
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
   return (
     <div>
-    {parts.map((part, index) => (
-      <Part key={index} name={part.name} exercises={part.exercises} />
-    ))}
-  </div>
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <StaticLine text="good" value={good} />
+          <StaticLine text="neutral" value={neutral} />
+          <StaticLine text="bad" value={bad} />
+          <StaticLine text="all" value={all} />
+          <StaticLine text="average" value={average} />
+          <StaticLine text="positive" value={`${positive} %`} />
+        </tbody>
+      </table>
+    </div>
   )
 }
 
-const Part = ({ name, exercises }) => {
-  return (
-    <p>{name} {exercises}</p>
-  )
-}
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
-    <p>Number of exercises {totalExercises}</p>
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      {good || neutral || bad ? <Statistics good={good} neutral={neutral} bad={bad} /> : <p>No feedback given</p>}
+    </div>
   )
 }
 
