58dc6f1d46e97238bb9f812fe2a4ff4a2b4bbb93
JuanTintos <juantintos@gmail.com>
2025-03-10 12:51:54 -0600

    Parte 2.11

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import Button from '@material-ui/core/Button';
 import Filter from './components/Filter';
 import Persons from './components/Persons';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -64,7 +66,7 @@ const App = () => {
         </div>
         <br></br>
         <div>
-          <Button variant="contained" color="primary" type="submit">Submit</Button>
+        <button type="submit">Save</button>
         </div>
       </form>
       <h2>Numbers</h2>
